f7533bab50ba8cf3334f52ab35f280deb0d95495
Abdullah-Sakib <abdullahsakib888@gmail.com>
2022-11-23 16:37:23 +0600

    testing

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+// Connect MongoDB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3booq2e.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
